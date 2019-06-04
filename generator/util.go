98c90eaf0e40bf88fa5328db0b551d3d4949e758
Sachin Kumar <skumar@clearstreet.io>
2019-06-04 19:43:16 -0400

    support initialisms

--- generator/util.go
+++ generator/util.go
@@ -1,14 +1,67 @@
 package generator
 
 import (
+	"regexp"
 	"strings"
 	"unicode"
 )
 
+var numberSequence = regexp.MustCompile(`([a-zA-Z])(\d+)([a-zA-Z]?)`)
+var numberReplacement = []byte(`$1 $2 $3`)
+
+func addWordBoundariesToNumbers(s string) string {
+	b := []byte(s)
+	b = numberSequence.ReplaceAll(b, numberReplacement)
+	return string(b)
+}
+
+// Converts a string to CamelCase
+func toCamelInitCase(s string, initCase bool) string {
+	s = addWordBoundariesToNumbers(s)
+	s = strings.Trim(s, " ")
+	n := ""
+	capNext := initCase
+	for _, v := range s {
+		if v >= 'A' && v <= 'Z' {
+			n += string(v)
+		}
+		if v >= '0' && v <= '9' {
+			n += string(v)
+		}
+		if v >= 'a' && v <= 'z' {
+			if capNext {
+				n += strings.ToUpper(string(v))
+			} else {
+				n += string(v)
+			}
+		}
+		if v == '_' || v == ' ' || v == '-' {
+			capNext = true
+		} else {
+			capNext = false
+		}
+	}
+	return n
+}
+
+// ToCamel converts a string to CamelCase
+func toCamel(s string) string {
+	return toCamelInitCase(s, true)
+}
+
 // ToPublicName returns a go-idiomatic public name. The Avro spec specifies names must start with [A-Za-z_] and contain [A-Za-z0-9_].
 // The golang spec says valid identifiers start with [A-Za-z_] and contain [A-Za-z0-9], but the first character must be [A-Z] for the field to be public.
 func ToPublicName(name string) string {
-	return strings.Title(strings.Trim(name, "_"))
+	// bleh: https://github.com/golang/go/wiki/CodeReviewComments#initialisms
+	if strings.HasSuffix(name, "_id") {
+		name = strings.TrimSuffix(name, "_id") + "ID"
+	}
+	if name == "id" {
+		return "ID"
+	}
+
+	return toCamel(name)
+	//return strings.Title(strings.Trim(name, "_"))
 }
 
 // ToSnake makes filenames snake-case, taken from https://gist.github.com/elwinar/14e1e897fdbe4d3432e1
